11baf62e4a9db5b7b4b5f9d52153e3e9d9a30045
Kataleko <katalekoweb@gmail.com>
2025-10-04 11:45:56 +0100

    state management with remeber and mutableStateOf

--- app/src/main/java/com/techvalon/stackmobile/MainActivity.kt
+++ app/src/main/java/com/techvalon/stackmobile/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.techvalon.stackmobile
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -20,8 +21,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,10 +33,16 @@ import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ExperimentalComposeApi
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -69,15 +78,75 @@ fun Home () {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        var email by remember { mutableStateOf("admin@admin.com") }
+        var password by remember { mutableStateOf("123456") }
+        val context = LocalContext.current
+
+        Text(
+            text = "Login Page",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(10.dp)
+        )
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text(text = "Email")
+            },
+            maxLines = 1
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text(text = "Password")
+            },
+            maxLines = 1
+        )
+
+        Button(
+            onClick = {
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(context, "Preencha todos o campos", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(context, "Login efectuado com sucesso", Toast.LENGTH_SHORT).show()
+                }
+            },
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(text = "Login")
+        }
+
+
+    }
+}
+
+/*
+@Composable
+fun Home () {
+    Column (
+        modifier = Modifier.fillMaxSize().background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Icon(
             imageVector = Icons.Rounded.Home,
             contentDescription = null,
             tint = Color.Blue,
-            modifier = Modifier.size(150.dp).padding(10.dp)
+            modifier = Modifier.size(100.dp).padding(10.dp)
         )
         Text("Juliao Kataleko", fontSize = 30.sp)
     }
 }
+ */
 
 /*
 @Composable
@@ -195,7 +264,7 @@ fun Home () {
 }
  */
 
-@Preview(name = "HomePreview", device = "id:tv_1080p")
+@Preview(name = "HomePreview", device = "id:Nexus 5")
 @Composable
 fun HomePreview () {
     Home()
